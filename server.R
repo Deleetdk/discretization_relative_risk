f7cd79e2106bb0701b325f39a739710c7f2aa971
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2015-08-22 04:00:15 -0500

    Initial publication
    An interactive visualization using Shiny for R to show the relationship
    between relative risk, absolute risk, effect size, discretization and
    predictor validity.

---
+++ server.R
@@ -0,0 +1,139 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+#functions
+#returns the beta of the error variance
+get_error = function(beta) {
+  beta_var = beta^2 #sq. beta to get % var accounted for
+  remain_var = 1 - beta_var #subtract from 1 to get % remain var
+  remain_beta = sqrt(remain_var) #sqrt to get the beta for the remainder
+  return(remain_beta)
+}
+
+#gets both the probability for event and non-event
+get_both_probs = function(x) {
+  return(c(x, 1 - x))
+}
+
+library(shiny)
+library(plyr)
+library(ggplot2)
+library(reshape)
+library(stringr)
+library(grid)
+
+shinyServer(function(input, output) {
+  
+  #reactive data, case-level
+  r_d = reactive({
+    #reproducible
+    set.seed(1)
+    n = 1e4
+    
+    #make latent pred var
+    d = data.frame(pred_con = rnorm(n))
+    
+    #make discrete predictor
+    d$pred_discrete = cut(d$pred_con, quantile(d$pred_con, seq(0, 1, length.out = input$n_groups + 1)), labels = F, include.lowest = T)
+    d$pred_discrete = as.factor(d$pred_discrete) #as factor
+    
+    #make latent outcome var
+    d$out_con = d$pred_con * input$cor + get_error(input$cor) * rnorm(n)
+    
+    #make binary outcome var
+    #modify add in the 
+    x_adjust = qnorm(input$base_rate_adj) #converts the z score adjustment to get the given probability
+    x_pnorm = pnorm(d$out_con + x_adjust) #adds the z score
+    
+    #roll the dice and get results
+    x_binary = numeric()
+    for (idx in seq_along(x_pnorm)) {
+      x_binary[idx] = sample(x = 1:0, #sample from 0 and 1
+                             size = 1, #get 1 sample
+                             prob = get_both_probs(x_pnorm[idx]) #with these probabilities
+      )
+    }
+    
+    #save to d
+    d$out_binary = x_binary
+    
+    return(d)
+  })
+  
+  r_base_rate = reactive({
+    #fetch d
+    d = r_d()
+    
+    #return baseline
+    return(mean(d$out_binary))
+  })
+  
+  #reactive data, group-level
+  r_d2 = reactive({
+    #fetch d
+    d = r_d()
+    
+    #proportion of true's in each category
+    d2 = ddply(.data = d, .variables = .(pred_discrete),
+               .fun = summarize,
+               prop = mean(out_binary))
+    
+    #relative risks
+    d2$RR_low = d2$prop / d2[1, "prop"]
+    d2$RR_high = d2$prop / d2[nrow(d2), "prop"]
+    central_row = floor(median(1:nrow(d2))) #get median value, round down. Otherwise if the number of groups is an even number, median will be a non-integer
+    d2$RR_central = d2$prop / d2[central_row, "prop"]
+    
+    return(d2)
+  })
+
+  #latent correlation
+  output$scatter <- renderPlot({
+    #fetch data
+    d = r_d()
+    
+    #plot
+    g = ggplot(d, aes(pred_con, out_con)) + geom_point(aes(color = pred_discrete)) + geom_smooth(method = lm, se = F) + xlab("Underlying predictor phenotype") + ylab("Underlying outcome phenotype") + scale_color_discrete(name = "Predictor group")
+    
+    return(g)
+  })
+  
+  #absolute risk by group
+  output$abs_risk = renderPlot({
+    #fetch data
+    d2 = r_d2()
+    base_rate = round(r_base_rate(), 3)
+    
+    #text grob
+    geom_text(x = length(d2$pred_discrete), y = .05, label = round(r_base_rate(), 3))
+    text = str_c("Base rate = ", base_rate)
+    grob = grobTree(textGrob(text, x = .95,  y = .1, hjust = 1,
+                             gp=gpar(col="black", fontsize=13)))
+    grob_bg = grobTree(rectGrob(gp=gpar(fill="white"), x = .89, y = .1, width = .15, height = .1))
+    
+    #plot prop by group
+    g = ggplot(d2, aes(pred_discrete, prop)) + geom_bar(stat = "identity") + xlab("Predictor group") + ylab("Absolute risk") + annotation_custom(grob_bg) + annotation_custom(grob)
+    
+    return(g)
+  })
+  
+  #relative risk by group
+  output$rel_risk = renderPlot({
+    #fetch data
+    d2 = r_d2()
+    
+    #to long form
+    d2$prop = NULL #remove
+    d3 = melt(d2)
+    d3$variable = factor(d3$variable, c("RR_low", "RR_central", "RR_high"))
+    
+    #plot
+    g = ggplot(d3, aes(pred_discrete, value)) + geom_bar(aes(group = variable, fill = variable), stat = "identity", position = "dodge") + xlab("Predictor group") + ylab("Relative risk") + geom_hline(yintercept = 1, linetype = "dashed") + scale_fill_discrete(name = "Comparison\ngroup", label = c("Lowest", "Central", "Highest"))
+    
+    return(g)
+  })
+})
---
+++ ui.R
@@ -0,0 +1,63 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Discretization and relative risk"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("n_groups",
+                  "Number of groups in the predictor:",
+                  min = 2,
+                  max = 21,
+                  value = 5),
+      sliderInput("cor",
+                  "Latent correlation:",
+                  min = 0,
+                  max = 1,
+                  value = .5,
+                  step = .05),
+      numericInput("base_rate_adj",
+                   "Base rate adjustment (non-linear):",
+                   value = .01,
+                   min = 0,
+                   max = 1,
+                   step = .001
+                  )
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      HTML("<p>Relative risks are frequently used in medicine to give an intuitive understanding of how strongly some experimental condition affected some kind of dichotomous (binary) outcome. However, interpreting the effect size from relative risk number is not as simple as one might think. This visualization attempts to explain why this is so.</p>",
+           "<p>Imagine we are measuring some kind of binary outcome. I will pretend it is whether a person was arrested a given year. Some people are more likely to get arrested, so one can speak of an underlying tendency, let's call it crime-proneness (assuming no bias in the justice system). This underlying trait is imperfectly related to arrests in a given year: a career criminal may fail to get arrested out of luck a given year and someone who decides to drive home from a party while intoxicated may get arrested for that despite not commiting any other criminal acts that year.</p>",
+           "<p>Let's say we are interested in predicting crime and have selected some predictor, such as aggressiveness. Suppose that we measure this in some kind of numerical way, but that we don't want to use the predictor as a numeric variable. Instead we divide people into groups based on their score on the aggressiveness measure. The scatter plot in the first tab shows how this might look.</p>",
+           "<p>Next up we calculate the proportion of individuals within each predictor group that were arrested that year. This gives us the absolute risk by group as shown in the second tab. If the event in question, getting arrested, is fairly rare, then absolute risks may not be very intuitive. Instead we may opt to calculate the relative risks, that is, how likely persons from the group are to be arrested compared to a particular group. One can choose which group to use as the comparison group depending on whether one wants numbers above, below or on both sides of 0. The third tab shows the relative risks for three choices of comparison group.</p>",
+           "<p>The relative risks, however, are a function not just of how good our predictor is, but also of the rarity of the event and the number of groups we have decided to split people into. A common choice focus on the relative risk of the group most likely to have the event happen to them and use the lowest probability group as the comparison group. With the default settings, persons from the highest probability group are about 7 times more likely to be arrested than those from the lowest probability group. Suppose however we had some agenda to push. Let's say we want to show that aggression is not strongly related to crime. If so, then we can lower the number of groups to 2 and see that the relative risk drops to 3. If however we are interested in showing the opposite, we could increase the number of groups. If we use 21 groups, then the highest probability group is about 12.5 times more likely to be arrested. In both cases the effect size is the same however. Thus, if one compares the relative risks from the highest probability group in studies that split the predictor into different numbers of groups, the results will not be comparable and one may be mislead to believe one predictor is better than another.</p>",
+           "<p>Try playing around with the settings to the left. You will see that if events are very rare, then relative risks are very large. If events are very common, then relative risks are always near 1. To be sure, the validity of the predictor also has a strong influence on the relative risks.</p>"),
+      tabsetPanel(
+        tabPanel("Underlying latent variables",
+                 plotOutput("scatter")
+        ),
+        tabPanel("Absolute risk",
+                 plotOutput("abs_risk")
+        ),
+        tabPanel("Relative risk",
+                 plotOutput("rel_risk"),
+                 HTML("<p>The dashed line shows relative risk of 1.</p>")
+        )
+      ),
+      HTML("Made by <a href='http://emilkirkegaard.dk'>Emil O. W. Kirkegaard</a> using <a href='http://shiny.rstudio.com/'/>Shiny</a> for <a href='http://en.wikipedia.org/wiki/R_%28programming_language%29'>R</a>. Source code available <a href='https://github.com/Deleetdk/discretization_relative_risk'>on Github</a>.")
+        
+    )
+    )
+  )
+)
